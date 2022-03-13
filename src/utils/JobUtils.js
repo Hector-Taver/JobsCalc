630f23a1291fc0ae256484b5732bbccf0e2fe1dc
Hector van Tol Taver <hectorvttaver@gmail.com>
2022-03-13 12:16:07 -0300

    changing Math.floor to Math.ceil in JobUtils.js

--- src/utils/JobUtils.js
+++ src/utils/JobUtils.js
@@ -10,7 +10,7 @@ module.exports = {
     const timeDiffInMs = dueDateInMs - Date.now();
     // Transformar ms em dias
     const dayInMs = 1000 * 60 * 60 * 24;
-    const dayDiff = Math.floor(timeDiffInMs / dayInMs);
+    const dayDiff = Math.ceil(timeDiffInMs / dayInMs);
 
     // Restam x dias
     return dayDiff;
